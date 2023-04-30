9bf2403b9730b59bd2c4c6fa065e5c80fd159551
iRevolutionDev <tavinho.daniel@gmail.com>
2023-04-30 04:37:08 -0300

    chore: add kotlin reflection dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.8")
     implementation("ch.qos.logback:logback-classic:1.2.9")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
     testImplementation(kotlin("test"))
 }
 
