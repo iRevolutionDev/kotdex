71bbfc00639c5fc745d74f4f65486fd90820b39e
iRevolutionDev <tavinho.daniel@gmail.com>
2023-05-01 00:39:43 -0300

    feat: add debug logger in importer

--- src/main/kotlin/com/kotdex/internal/reflection/Importer.kt
+++ src/main/kotlin/com/kotdex/internal/reflection/Importer.kt
@@ -1,11 +1,16 @@
 package com.kotdex.internal.reflection
 
 import com.kotdex.annotations.Discord
+import com.kotdex.internal.proxies.SLF4J
 import org.reflections.Reflections
+
+val logger by SLF4J("Importer")
 
 fun import(packagePath: String) {
     val reflections = Reflections(packagePath)
 
     val classes = reflections.getTypesAnnotatedWith(Discord::class.java)
     ReflectionUtils.discordClasses.addAll(classes)
+
+    logger.debug("Imported ${classes.size} classes from $packagePath")
 }
