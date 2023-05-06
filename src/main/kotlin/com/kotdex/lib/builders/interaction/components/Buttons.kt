e3d53af6ba0aad1ef6b4ce0024541502ed2b1301
7SindDev <115845297+7SindDev@users.noreply.github.com>
2023-05-06 04:30:41 -0300

    docs(builders): add Button builders docs

--- src/main/kotlin/com/kotdex/lib/builders/interaction/components/Buttons.kt
+++ src/main/kotlin/com/kotdex/lib/builders/interaction/components/Buttons.kt
@@ -4,6 +4,16 @@ import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
 
+/**
+ * Returns a new [Button] instance with the provided properties.
+ *
+ * @param id the unique identifier of the button
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @param style the visual style of the button
+ * @param disabled whether the button is disabled or not
+ * @return a new [Button] instance
+ */
 fun button(
     id: String,
     label: String?,
@@ -14,22 +24,62 @@ fun button(
     return Button.of(style, id, label, emoji).withDisabled(disabled)
 }
 
+/**
+ * Returns a new primary [Button] instance with the provided properties.
+ *
+ * @param id the unique identifier of the button
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @return a new primary [Button] instance
+ */
 fun primary(id: String, label: String, emoji: Emoji? = null): Button {
     return button(id, label, emoji, ButtonStyle.PRIMARY)
 }
 
+/**
+ * Returns a new secondary [Button] instance with the provided properties.
+ *
+ * @param id the unique identifier of the button
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @return a new secondary [Button] instance
+ */
 fun secondary(id: String, label: String, emoji: Emoji? = null): Button {
     return button(id, label, emoji, ButtonStyle.SECONDARY)
 }
 
+/**
+ * Returns a new success [Button] instance with the provided properties.
+ *
+ * @param id the unique identifier of the button
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @return a new success [Button] instance
+ */
 fun success(id: String, label: String, emoji: Emoji? = null): Button {
     return button(id, label, emoji, ButtonStyle.SUCCESS)
 }
 
+/**
+ * Returns a new danger [Button] instance with the provided properties.
+ *
+ * @param id the unique identifier of the button
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @return a new danger [Button] instance
+ */
 fun danger(id: String, label: String, emoji: Emoji? = null): Button {
     return button(id, label, emoji, ButtonStyle.DANGER)
 }
 
+/**
+ * Returns a new link [Button] instance with the provided properties.
+ *
+ * @param url the URL to link to when the button is clicked
+ * @param label the label text of the button
+ * @param emoji the emoji icon to display next to the label
+ * @return a new link [Button] instance
+ */
 fun link(url: String, label: String, emoji: Emoji? = null): Button {
     return button(url, label, emoji, ButtonStyle.LINK)
 }
