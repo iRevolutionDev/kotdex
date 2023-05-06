72ffe6df0b7a8f58949c3d791deaa2b0886f1f26
7SindDev <115845297+7SindDev@users.noreply.github.com>
2023-05-06 04:31:44 -0300

    docs(builders): add EntitySelectMenu builder docs

--- src/main/kotlin/com/kotdex/lib/builders/interaction/components/EntitySelectMenu.kt
+++ src/main/kotlin/com/kotdex/lib/builders/interaction/components/EntitySelectMenu.kt
@@ -4,6 +4,12 @@ import com.kotdex.lib.builders.IBuilder
 import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu
 import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu.SelectTarget
 
+/**
+ * A builder for creating [EntitySelectMenu]s.
+ *
+ * @property customId The custom ID to use for the menu.
+ * @property type The select target to use for the menu.
+ */
 class EntitySelectMenuBuilder(
     private val customId: String,
     private val type: SelectTarget
@@ -11,6 +17,9 @@ class EntitySelectMenuBuilder(
     var placeholder: String? = null
     var requiredRange: IntRange = 1..1
 
+    /**
+     * Builds an [EntitySelectMenu] with the properties set on this builder.
+     */
     override fun build(): EntitySelectMenu {
         return EntitySelectMenu.create(customId, type)
             .setPlaceholder(placeholder)
@@ -19,6 +28,25 @@ class EntitySelectMenuBuilder(
     }
 }
 
+/**
+ * Creates an [EntitySelectMenu] with the given custom ID and select target.
+ * The placeholder and required range can also be set using the builder methods.
+ *
+ * Example usage:
+ *
+ * ```kotlin
+ * val selectMenu = entityselectmenu("menu-1", SelectTarget.USERS) {
+ *     placeholder = "Select a user"
+ *     requiredRange = 1..3
+ * }
+ * ```
+ *
+ * @param customId The custom ID to use for the menu.
+ * @param type The select target to use for the menu.
+ * @param block A lambda that takes an [EntitySelectMenuBuilder] as its receiver, allowing
+ * the builder methods to be called to set additional properties for the menu.
+ * @return The created [EntitySelectMenu].
+ */
 fun entityselectmenu(
     customId: String,
     type: SelectTarget,
